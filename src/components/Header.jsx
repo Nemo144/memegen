ec65fce4f56d774e6558fcce1f5cc7f5ffc48ce3
Nnamdi Okeke <onnamdialex@gmail.com>
2022-06-09 20:21:54 +0100

    :sparkles: header completed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import { images } from "../constants";
 
 export const Header = () => {
   return (
     <div>
-      <h1>Header</h1>
+      <header className="h-20  bg-purple-700 ">
+        <div className="flex justify-between items-end">
+          <div className="flex p-6">
+            <img src={images.troll} alt="troll" className="h-8" />
+            <h1 className="text-white text-xl ml-2">Meme Generator</h1>
+          </div>
+
+          <div className="flex ">
+            <h2 className="text-white pb-8 pr-12">Memes of Life - hack!</h2>
+          </div>
+        </div>
+      </header>
     </div>
   );
 };
---
+++ src/constants/images.js
@@ -0,0 +1,5 @@
+import troll from "../assets/troll.png";
+
+export default {
+  troll,
+};
