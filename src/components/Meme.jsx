c89c2ec6efed3fbe29fee6aca84170b08acd590e
Nnamdi Okeke <onnamdialex@gmail.com>
2022-06-10 17:52:12 +0100

    :art: color fix

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -6,25 +6,24 @@ export const Meme = () => {
       <div className="flex m-10 p-5">
         <input
           type="text"
-          className="w-full rounded-lg mr-6 bg-transparent border py-4 px-4 text-black outline-none focus:ring-2 focus:ring-gray-200"
+          className="w-full rounded-lg border-zinc-300 mr-6 bg-transparent border py-4 px-4 text-black outline-none focus:ring-2 focus:ring-gray-200"
           placeholder="up"
           name="name"
         />
 
         <input
           type="text"
-          className="w-full rounded-lg bg-transparent border py-4 px-4 text-black outline-none focus:ring-2 focus:ring-gray-200"
+          className="w-full rounded-lg border-zinc-300 bg-transparent border py-4 px-4 text-black outline-none focus:ring-2 focus:ring-gray-200"
           placeholder="down"
           name="name"
         />
       </div>
-      <div className="m-14">
+      <div className="m-14 my-11">
         <button
           className="w-full rounded-lg bg-purple-700 h-16 font-bold px-4 text-white outline-none focus:ring-2 focus:ring-gray-200"
           placeholder="Name"
-          name="name"
         >
-          Get a new meme image 🏞{" "}
+          Get a new meme image 🖼{" "}
         </button>
       </div>
     </div>
