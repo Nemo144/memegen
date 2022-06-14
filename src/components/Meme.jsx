1350007b930e6b23499c3fcccb16e12a52fd72b9
Nnamdi Okeke <onnamdialex@gmail.com>
2022-06-14 23:08:31 +0100

    :construction: meme wip

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -7,17 +7,16 @@ export const Meme = () => {
         <input
           type="text"
           className="w-full rounded-lg border-zinc-300 mr-6 bg-transparent border py-4 px-4 text-black outline-none focus:ring-2 focus:ring-gray-200"
-          placeholder="up"
-          name="name"
+          placeholder="Top text"
         />
 
         <input
           type="text"
           className="w-full rounded-lg border-zinc-300 bg-transparent border py-4 px-4 text-black outline-none focus:ring-2 focus:ring-gray-200"
-          placeholder="down"
-          name="name"
+          placeholder="Bottom text"
         />
       </div>
+
       <div className="m-14 my-11">
         <button
           className="w-full rounded-lg bg-purple-700 h-16 font-bold px-4 text-white outline-none focus:ring-2 focus:ring-gray-200"
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,10 @@
 import images from "./images";
 
 export { images };
+
+// function getMemeImage() {
+//     const memesArray = memesData.data.memes
+//     const randomNumber = Math.floor(Math.random() * memesArray.length)
+//     url = memesArray[randomNumber].url
+//     console.log(url)
+// }
