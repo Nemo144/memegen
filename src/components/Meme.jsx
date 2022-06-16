66d86b1d3fbdfe238598978c6c28ecde6023b0b6
Nnamdi Okeke <onnamdialex@gmail.com>
2022-06-16 16:16:46 +0100

    :sparkles: add the meme button generator

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,6 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
+import memesData from "../constants/memesData";
 
 export const Meme = () => {
+  const [memeImage, setMemeImage] = useState("");
+
+  function getMemeImage() {
+    const memesArray = memesData.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    setMemeImage(memesArray[randomNumber].url);
+  }
+
   return (
     <div>
       <div className="flex m-10 p-5">
@@ -21,9 +30,14 @@ export const Meme = () => {
         <button
           className="w-full rounded-lg bg-purple-700 h-16 font-bold px-4 text-white outline-none focus:ring-2 focus:ring-gray-200"
           placeholder="Name"
+          onClick={getMemeImage}
         >
           Get a new meme image 🖼{" "}
         </button>
+      </div>
+
+      <div className="flex justify-center items-center">
+        <img src={memeImage} alt="meme" className="w-max" />
       </div>
     </div>
   );
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,10 +1,3 @@
 import images from "./images";
 
 export { images };
-
-// function getMemeImage() {
-//     const memesArray = memesData.data.memes
-//     const randomNumber = Math.floor(Math.random() * memesArray.length)
-//     url = memesArray[randomNumber].url
-//     console.log(url)
-// }
